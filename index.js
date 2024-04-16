eae2562336fed5457ff75c72edf2646dd6780e5f
KofE <bhiushi6564@gmail.com>
2024-04-16 15:50:38 +0500

    Alter default of form. get info from API

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const API_key = '9282dbb4277319a79bd6c35abc11187c'
-async function getWeather(){
-    let url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_key}`
+let cityName = 'Astana'
+let limit = 1
+async function getWeather(lat,lon) {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`
     const response = await fetch(url)
     const data = await response.json()
-    console.log(data['city']['name'])
-    console.log(data['list'][0]['main'])
+    console.log(data)
+    console.log(data)
+}
+
+async function getLetLatForCity(){
+    let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${API_key}`
+    const response = await fetch(url)
+    const data = await response.json()
+    getWeather(data[0]['lat'],data[0]['lon'])
 }
 
 
-
-getWeather()
+getLetLatForCity()
